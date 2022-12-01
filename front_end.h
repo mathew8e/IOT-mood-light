8d2a17ccf2b4c63112279115d56ea3db4b0cae24
mathew8e <123kumatan@gmail.com>
2022-12-01 20:44:36 +0100

    cleaned it up and added debounce fun mode still working wierd

--- front_end.h
+++ front_end.h
@@ -7,18 +7,28 @@ const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html><head>
   <title>Light interface</title>
   <style>
-    
+    .mainControll {
+      text-align:center;
+    }
   </style>
   </head>
   <body>
-    <h1> Led Lamp </h1>
-    <a href="/turn"> <button>%STATE%</button> </a>
-    
-    <p> teplota: %TEMP% </p>
-    <p> humidity: %HUMID% </p>
-    <p> light intensity: %LIT% </p>
+    <div class="mainControll">
+      <h1> Led Lamp </h1>
+      <a href="/turn"> <button>%STATE%</button> </a>
+      <br>
+      <br>
+      <a href="/funMode"> <button>Fun Mode</button> </a>
+      
+      <p> teplota: %TEMP% </p>
+      <p> humidity: %HUMID% </p>
+      <p> light intensity: %LIT% </p>
 
-    <a href="/"> <button> UPDATE </button> </a>
+      <a href="/"> <button> UPDATE </button> </a>
+
+      <br>
+      version 1.0.1
+    <div>
   </body>
 </html>)rawliteral";
 
